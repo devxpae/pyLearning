40afb6d19aec8e5ea33dd69b8a1d1ae4b46d2441
Alexander Xpae <social.xpae@gmail.com>
2022-11-21 00:00:02 +0300

    Строки (Дополнение)

--- basicDataStructures/strings.py
+++ basicDataStructures/strings.py
@@ -1,3 +1,23 @@
+someStringA = 'qwertyuiop'
+someStringB = 'asdfghjkl'
+
 print('Название')
 print("Подходит для написания абзацев, большших фрагментов текста.")
-print("ООО {companyName}".format(companyName = '"Название компании"'))
+print("ООО {companyName} — №1 на рынке IT.".format(companyName = '"Название компании"'))
+
+print("\nОператоры сравнения:")
+print("Длина объекта someStringA — {valueA}, а someStringB — {valueB}".format(valueA = len(someStringA), valueB = len(someStringB)))
+print("({valueA} > {valueB}): {result}" .format(valueA = someStringA, valueB = someStringB, result = someStringA > someStringB))
+print("({valueA} >= {valueB}): {result}".format(valueA = someStringA, valueB = someStringB, result = someStringA >= someStringB))
+print("({valueA} < {valueB}): {result}" .format(valueA = someStringA, valueB = someStringB, result = someStringA < someStringB))
+print("({valueA} <= {valueB}): {result}".format(valueA = someStringA, valueB = someStringB, result = someStringA <= someStringB))
+print("({valueA} == {valueB}): {result}".format(valueA = someStringA, valueB = someStringB, result = someStringA == someStringB))
+print("({valueA} != {valueB}): {result}".format(valueA = someStringA, valueB = someStringB, result = someStringA != someStringB))
+
+print("\n* — с объектами типа String (str) возможно производить те же операции, что и с объектами типа Numbers (int, float, ...).")
+
+print("\nИндексация строк:")
+print("Вывод 1 символ объекта someStringA: {result}". format(result = someStringA[0]))
+print("Вывод с 1 по 4 символ объекта someStringA: {result}". format(result = someStringA[0:4]))
+print("Вывод символов объекта someStringA через 1: {result}". format(result = someStringA[::2]))
+print("Вывод символов объекта someStringA в обратном порядке: {result}". format(result = someStringA[::-1]))
