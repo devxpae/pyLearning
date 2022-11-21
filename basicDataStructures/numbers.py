cbdd1c0e50a7eac9000934344e6bd37f6631fb78
Alexander Xpae <social.xpae@gmail.com>
2022-11-21 14:34:22 +0300

    Правка кода (Правка синтаксиса)

--- basicDataStructures/numbers.py
+++ basicDataStructures/numbers.py
@@ -1,38 +1,38 @@
-someValueA = 5
-someValueB = 25
-someList = [1, 2, 3, 4, 5]
+some_value_a = 5
+some_value_b = 25
+some_list = [1, 2, 3, 4, 5]
 
 print("Арифметические операторы:")
-print("Добавление: {valueA} + {valueB} = {result}"  .format(valueA = someValueA, valueB = someValueB, result = someValueA + someValueB))
-print("Вычитание: {valueA} - {valueB} = {result}"   .format(valueA = someValueA, valueB = someValueB, result = someValueA - someValueB))
-print("Умножение: {valueA} * {valueB} = {result}"   .format(valueA = someValueA, valueB = someValueB, result = someValueA * someValueB))
-print("Деление: {valueA} / {valueB} = {result}"     .format(valueA = someValueA, valueB = someValueB, result = someValueA / someValueB))
+print("Добавление: {value_a} + {value_b} = {result}"  .format(value_a = some_value_a, value_b = some_value_b, result = some_value_a + some_value_b))
+print("Вычитание: {value_a} - {value_b} = {result}"   .format(value_a = some_value_a, value_b = some_value_b, result = some_value_a - some_value_b))
+print("Умножение: {value_a} * {value_b} = {result}"   .format(value_a = some_value_a, value_b = some_value_b, result = some_value_a * some_value_b))
+print("Деление: {value_a} / {value_b} = {result}"     .format(value_a = some_value_a, value_b = some_value_b, result = some_value_a / some_value_b))
 print("Модуль (нечетное значение): 5 % 2 = {result}".format(result = 5 % 2))
 print("Модуль (четное значение): 4 % 2 = {result}"  .format(result = 4 % 2))
-print("Степень: {valueA} ** {valueB} = {result}"    .format(valueA = someValueA, valueB = someValueB, result = someValueA ** someValueB))
+print("Степень: {value_a} ** {value_b} = {result}"    .format(value_a = some_value_a, value_b = some_value_b, result = some_value_a ** some_value_b))
 print("Деление с остатком: 3 // 2 = {result}"       .format(result = 3 // 2))
 print("* — операторы присваивания работают аналогичным образом.")
 
 print("\nОператоры сравнения:")
-print("({valueA} > {valueB}): {result}" .format(valueA = someValueA, valueB = someValueB, result = someValueA > someValueB))
-print("({valueA} >= {valueB}): {result}".format(valueA = someValueA, valueB = someValueB, result = someValueA >= someValueB))
-print("({valueA} < {valueB}): {result}" .format(valueA = someValueA, valueB = someValueB, result = someValueA < someValueB))
-print("({valueA} <= {valueB}): {result}".format(valueA = someValueA, valueB = someValueB, result = someValueA <= someValueB))
-print("({valueA} == {valueB}): {result}".format(valueA = someValueA, valueB = someValueB, result = someValueA == someValueB))
-print("({valueA} != {valueB}): {result}".format(valueA = someValueA, valueB = someValueB, result = someValueA != someValueB))
+print("({value_a} > {value_b}): {result}" .format(value_a = some_value_a, value_b = some_value_b, result = some_value_a > some_value_b))
+print("({value_a} >= {value_b}): {result}".format(value_a = some_value_a, value_b = some_value_b, result = some_value_a >= some_value_b))
+print("({value_a} < {value_b}): {result}" .format(value_a = some_value_a, value_b = some_value_b, result = some_value_a < some_value_b))
+print("({value_a} <= {value_b}): {result}".format(value_a = some_value_a, value_b = some_value_b, result = some_value_a <= some_value_b))
+print("({value_a} == {value_b}): {result}".format(value_a = some_value_a, value_b = some_value_b, result = some_value_a == some_value_b))
+print("({value_a} != {value_b}): {result}".format(value_a = some_value_a, value_b = some_value_b, result = some_value_a != some_value_b))
 
 print("\nЛогические операторы:")
-print("({valueA} > {valueB} and {valueA} < {valueB}): {result}" .format(valueA = someValueA, valueB = someValueB, 
-                                                                result = someValueA > someValueB and someValueA < someValueB))
-print("(not ({valueA} > {valueB} and {valueA} < {valueB})): {result}" .format(valueA = someValueA, valueB = someValueB, 
-                                                                result = not (someValueA > someValueB and someValueA < someValueB)))
-print("({valueA} > {valueB} or {valueA} < {valueB}): {result}" .format(valueA = someValueA, valueB = someValueB, 
-                                                                result = someValueA > someValueB or someValueA < someValueB))
+print("({value_a} > {value_b} and {value_a} < {value_b}): {result}" .format(value_a = some_value_a, value_b = some_value_b, 
+                                                                result = some_value_a > some_value_b and some_value_a < some_value_b))
+print("(not ({value_a} > {value_b} and {value_a} < {value_b})): {result}" .format(value_a = some_value_a, value_b = some_value_b, 
+                                                                result = not (some_value_a > some_value_b and some_value_a < some_value_b)))
+print("({value_a} > {value_b} or {value_a} < {value_b}): {result}" .format(value_a = some_value_a, value_b = some_value_b, 
+                                                                result = some_value_a > some_value_b or some_value_a < some_value_b))
                                                                 
 print("\nОператоры членства:")
-print("({valueA} in {list}): {result}"      .format(valueA = someValueA, list = someList, result = someValueA in someList))
-print("({valueA} not in {list}): {result}"  .format(valueA = someValueA, list = someList, result = someValueA not in someList))
+print("({value_a} in {list}): {result}"      .format(value_a = some_value_a, list = some_list, result = some_value_a in some_list))
+print("({value_a} not in {list}): {result}"  .format(value_a = some_value_a, list = some_list, result = some_value_a not in some_list))
 
 print("\nОператоры идентификации:")
-print("({valueA} is {valueB}): {result}"        .format(valueA = someValueA, valueB = someValueB, result = someValueA is someValueB))
-print("({valueA} is not {valueB}): {result}"    .format(valueA = someValueA, valueB = someValueB, result = someValueA is not someValueB))
+print("({value_a} is {value_b}): {result}"        .format(value_a = some_value_a, value_b = some_value_b, result = some_value_a is some_value_b))
+print("({value_a} is not {value_b}): {result}"    .format(value_a = some_value_a, value_b = some_value_b, result = some_value_a is not some_value_b))
--- basicDataStructures/strings.py
+++ basicDataStructures/strings.py
@@ -1,26 +1,26 @@
-someStringA = 'qwertyuiop'
-someStringB = 'asdfghjkl'
+some_string_a = 'qwertyuiop'
+some_string_b = 'asdfghjkl'
 
 print('Название')
 print("Подходит для написания абзацев, большших фрагментов текста.")
 print("ООО {companyName} — №1 на рынке IT.".format(companyName = '"Название компании"'))
 
 print("\nОператоры сравнения:")
-print("Длина объекта someStringA — {valueA}, а someStringB — {valueB}".format(valueA = len(someStringA), valueB = len(someStringB)))
-print("({valueA} > {valueB}): {result}" .format(valueA = someStringA, valueB = someStringB, result = someStringA > someStringB))
-print("({valueA} >= {valueB}): {result}".format(valueA = someStringA, valueB = someStringB, result = someStringA >= someStringB))
-print("({valueA} < {valueB}): {result}" .format(valueA = someStringA, valueB = someStringB, result = someStringA < someStringB))
-print("({valueA} <= {valueB}): {result}".format(valueA = someStringA, valueB = someStringB, result = someStringA <= someStringB))
-print("({valueA} == {valueB}): {result}".format(valueA = someStringA, valueB = someStringB, result = someStringA == someStringB))
-print("({valueA} != {valueB}): {result}".format(valueA = someStringA, valueB = someStringB, result = someStringA != someStringB))
+print("Длина объекта some_string_a — {valueA}, а some_string_b — {valueB}".format(valueA = len(some_string_a), valueB = len(some_string_b)))
+print("({valueA} > {valueB}): {result}" .format(valueA = some_string_a, valueB = some_string_b, result = some_string_a > some_string_b))
+print("({valueA} >= {valueB}): {result}".format(valueA = some_string_a, valueB = some_string_b, result = some_string_a >= some_string_b))
+print("({valueA} < {valueB}): {result}" .format(valueA = some_string_a, valueB = some_string_b, result = some_string_a < some_string_b))
+print("({valueA} <= {valueB}): {result}".format(valueA = some_string_a, valueB = some_string_b, result = some_string_a <= some_string_b))
+print("({valueA} == {valueB}): {result}".format(valueA = some_string_a, valueB = some_string_b, result = some_string_a == some_string_b))
+print("({valueA} != {valueB}): {result}".format(valueA = some_string_a, valueB = some_string_b, result = some_string_a != some_string_b))
 
 print("\n* — с объектами типа String (str) возможно производить те же операции, что и с объектами типа Numbers (int, float, ...).")
 
 print("\nИндексация строк:")
-print("Вывод 1 символ объекта someStringA: {result}"                    .format(result = someStringA[0]))
-print("Вывод с 1 по 4 символ объекта someStringA: {result}"             .format(result = someStringA[0:4]))
-print("Вывод символов объекта someStringA через 1: {result}"            .format(result = someStringA[::2]))
-print("Вывод символов объекта someStringA в обратном порядке: {result}" .format(result = someStringA[::-1]))
+print("Вывод 1 символ объекта some_string_a: {result}"                    .format(result = some_string_a[0]))
+print("Вывод с 1 по 4 символ объекта some_string_a: {result}"             .format(result = some_string_a[0:4]))
+print("Вывод символов объекта some_string_a через 1: {result}"            .format(result = some_string_a[::2]))
+print("Вывод символов объекта some_string_a в обратном порядке: {result}" .format(result = some_string_a[::-1]))
 
 print("\nКодирование / Декодирование:")
 some_string_c = 'йцукен'.encode()
